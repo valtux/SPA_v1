afeed6e1e87f6e1f7be0362227bd72fcf938bf36
tuxval <valeriashanahan@gmail.com>
2018-02-10 17:49:59 -0300

    creacion del primer servicio heroes.services

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {HeroesService, Heroe} from "../../servicios/heroes.service";
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  // private heroes:Heroe [] = [];
+  heroes:Heroe [] = [];
+  constructor(private _heroesService:HeroesService) {
 
-  ngOnInit() {
+      console.log("constructor");
+      }
+
+  ngOnInit(){
+    this.heroes = this._heroesService.getHeroe();
+    console.log("ngOnInit");
+    console.log(this.heroes);
   }
+
+
 
 }
